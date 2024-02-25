4640cb0c95610b8940953ecb3202dab0e4bb093f
AlexHorbatiuk <a2022@MBP-2022.fritz.box>
2024-02-25 14:16:44 +0100

    add animation for CubeTwo

--- src/components/Cubes/CubeTwo/index.jsx
+++ src/components/Cubes/CubeTwo/index.jsx
@@ -1,16 +1,59 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './index.scss';
 
 export const CubeTwo = () => {
 	const sideCount = 19; // Количество сторон
-	const sides = Array(sideCount).fill().map((_, index) => index + 1);
+	const [currentSideIndex, setCurrentSideIndex] = useState(0);
+	const [isAddingClasses, setIsAddingClasses] = useState(true);
+	const animationSpeed = 200; // Скорость анимации (количество кадров в секунду)
+	const framesPerSide = Math.ceil(animationSpeed / sideCount); // Количество кадров на каждую сторону
+
+	useEffect(() => {
+		let frameCount = 0;
+		let animationFrameId;
+
+		const animate = () => {
+			if (frameCount === framesPerSide) {
+				frameCount = 0;
+
+				if (isAddingClasses) {
+					if (currentSideIndex < sideCount) {
+						setCurrentSideIndex((prevIndex) => prevIndex + 1);
+					} else {
+						setIsAddingClasses(false);
+					}
+				} else {
+					if (currentSideIndex > 0) {
+						setCurrentSideIndex((prevIndex) => prevIndex - 1);
+					} else {
+						setIsAddingClasses(true);
+					}
+				}
+			}
+
+			frameCount++;
+			animationFrameId = requestAnimationFrame(animate);
+		};
+
+		// Запуск анимации
+		animationFrameId = requestAnimationFrame(animate);
+
+		// Отмена анимации при размонтировании компонента
+		return () => cancelAnimationFrame(animationFrameId);
+	}, [currentSideIndex, sideCount, isAddingClasses, framesPerSide]);
+
 
 	return (
 		<div className="earth-wrapper">
 			<div className="earth">
 				<span>3D Effect</span>
-				{sides.map((side) => (
-					<div key={side} className={`side side-${side}`}></div>
+				{Array.from({ length: sideCount }).map((_, index) => (
+					<div
+						key={index + 1}
+						className={`side side-${index + 1} ${
+							index < currentSideIndex ? 'newClass' : ''
+						}`}
+					></div>
 				))}
 			</div>
 		</div>
